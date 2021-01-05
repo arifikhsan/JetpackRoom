b88f6384539ad42c6dfb087eb12f4583f403dbdc
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2021-01-05 11:00:35 +0700

    200

--- app/src/main/java/com/arifikhsan/jetpackroom/database/NoteRoomDatabase.kt
+++ app/src/main/java/com/arifikhsan/jetpackroom/database/NoteRoomDatabase.kt
@@ -41,7 +41,7 @@ abstract class NoteRoomDatabase : RoomDatabase() {
         fun add() {
             Executors.newSingleThreadExecutor().execute {
                 val list: MutableList<Note> = ArrayList()
-                for (i in 0..9) {
+                for (i in 0..200) {
                     val dummyNote = Note()
                     dummyNote.title = "Tugas $i"
                     dummyNote.description = "Belajar Modul $i"
